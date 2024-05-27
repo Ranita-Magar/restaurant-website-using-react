7ca706f9b42e2e845d5dc8a3c89ccedaf1573f16
Ranita-Magar <87315393+Ranita-Magar@users.noreply.github.com>
2024-05-27 18:22:50 +0545

    Creating Responsive Laurel section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import './Laurels.css';
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
+import "./Laurels.css";
+
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => {
+  return (
+    <div className="app__laurels_awards-card">
+      <img src={imgUrl} alt="award" />
+      <div className="app__laurels_awards-card_content">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {subtitle}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app_wrapper_info">
+      <SubHeading title="Awards & recognition" style={{ }} />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
+    </div>
+
+    <div className="app_wrapper_img">
+      <img src={images.laurels} alt="laurels" />
+    </div>
   </div>
 );
 
